448b14084cad2e3db964fed8c295401a538163dc
Lucas Vinicius <lucasv2224@outlook.com>
2025-12-26 11:06:09 -0300

    feat: adiciona rotas de usuários e controlador para criação de usuários

---
+++ src/routes/users-router.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { UserController } from "@/controllers/user-controller";
+
+const usersRouter = Router();
+const usersController = new UserController();
+
+usersRouter.post("/", usersController.create);
+
+export { usersRouter };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import { errorHandler } from "./middlewares/error-handler";
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(routes);
 app.use(errorHandler);
 
 app.listen(3333, () => console.log("Servidor rodando na porta 3333"));
