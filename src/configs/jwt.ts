f9f1e9e24f98d243773b24cebe268071ec65119d
Lucas Vinicius <lucasv2224@outlook.com>
2025-12-26 17:43:32 -0300

    feat: implementa criação de sessão com validação de email e senha

---
+++ src/configs/jwt.ts
@@ -0,0 +1,6 @@
+export const jwtConfig = {
+  jwt: {
+    secret: "lucas",
+    expiresIn: "1d",
+  },
+};
--- src/controllers/session-controller.ts
+++ src/controllers/session-controller.ts
@@ -1,8 +1,40 @@
 import { Request, Response } from "express";
+import { z } from "zod";
+import prisma from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
+import { compare } from "bcrypt";
+import { jwtConfig } from "@/configs/jwt";
+import { sign } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 class SessionController {
   async create(req: Request, res: Response) {
-    res.json({ message: "ok" });
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string(),
+    });
+
+    const { email, password } = bodySchema.parse(req.body);
+
+    const user = await prisma.user.findFirst({ where: { email } });
+
+    if (!user) {
+      throw new AppError("Email e/ou senha inválido!", 401);
+    }
+
+    const matchedPassword = await compare(password, user.password);
+
+    if (!matchedPassword) {
+      throw new AppError("Email e/ou senha inválidos!", 401);
+    }
+
+    const { secret, expiresIn } = jwtConfig.jwt;
+    const token = jwt.sign({ role: user.role }, secret, {
+      expiresIn,
+      subject: String(user.id),
+    });
+
+    res.json({ token });
   }
 }
 
