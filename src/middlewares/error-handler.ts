397178a9ed1dec9c79e56853c6693dd3f4d68bdc
Lucas Vinicius <lucasv2224@outlook.com>
2025-12-26 10:21:51 -0300

    chore: adiciona tratamento de erros com AppError e ZodError, e atualiza dependências

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/AppError";
+import { ZodError } from "zod";
+
+export function errorHandler(
+  error: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+
+  if (error instanceof ZodError) {
+    return res.status(400).json({
+      message: "Validation error!",
+      issues: error.format(),
+    });
+  }
+
+  res.status(500).json({
+    status: "error",
+    message: error.message || "Erro interno no servidor",
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
+app.use(errorHandler);
 
 app.listen(3333, () => console.log("Servidor rodando na porta 3333"));
