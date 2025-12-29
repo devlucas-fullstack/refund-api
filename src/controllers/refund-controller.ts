5286b4c26fb08314e056d2f1b05551a6beebc95f
Lucas Vinicius <lucasv2224@outlook.com>
2025-12-29 13:02:21 -0300

    feat: adiciona validação de dados e estrutura para criação de reembolso

--- src/controllers/refund-controller.ts
+++ src/controllers/refund-controller.ts
@@ -1,7 +1,41 @@
+import prisma from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
 import { Request, Response } from "express";
+import { z } from "zod";
+
+const categoryEnum = z.enum([
+  "food",
+  "others",
+  "transport",
+  "accommodation",
+  "services",
+]);
 
 class RefundController {
   async create(req: Request, res: Response) {
+    const bodySchema = z.object({
+      name: z.string().min(1, { message: "Informe o nome da solicitação!" }),
+      category: categoryEnum,
+      amount: z.number().positive(),
+      filename: z.string().min(20),
+    });
+
+    const { name, category, amount, filename } = bodySchema.parse(req.body);
+
+    if (!req.user.id) {
+      throw new AppError("Não autorizado!", 401);
+    }
+
+    await prisma.refund.create({
+      data: {
+        name,
+        category,
+        amount,
+        filename,
+        userId: req.user.id,
+      },
+    });
+
     res.json({ message: "ok" });
   }
 }
